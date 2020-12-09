d0d2ab6f16626bd110575fe5fd3f8feedcf3ecf6
Jeff Borongan <jeffborongan@gmail.com>
2020-12-09 21:11:35 +0800

    [Predator Ball - Jeff] - Made surrounding environments bouncy

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * Time.deltaTime * movementSpeed;
+    }
+}
