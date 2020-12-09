0a00bdb98b81b219211eba85e8989ddffc366b8a
Jeff Borongan <jeffborongan@gmail.com>
2020-12-09 22:25:40 +0800

    [Predator Ball - Jeff] - Added the Enemy AI Chasing Code

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 100f;
+    public float nextWaypointDistance = 0.5f;
+    Path path;
+    int currentWaypoint = 0;
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating(nameof(UpdatePath), 0f, 0.5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (path == null)
+            return;
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        rb.velocity = direction * speed * Time.deltaTime;
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+}
