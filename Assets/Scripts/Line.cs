5db7c2c90ee2236c6994a824eec321968abb89e4
Nivelle Mendiola <mendiolanivelle@gmail.com>
2021-03-26 10:41:47 +0800

    [Ink -Nivelle] - Line drawing is now replicated to one another

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -1,72 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using Mirror;
-using System.Collections;
 
 public class Line : NetworkBehaviour
 {
-	public LineRenderer lineRenderer;
-	public EdgeCollider2D edgeCollider;
-	public EdgeCollider2D edgeColliderTriggerable;
-	[HideInInspector] public List<Vector2> fingerPositions;
+
+	public List<Vector2> fingerPositions;
 	[HideInInspector] public int pointsCount = 0;
-	float linePointsMinDistance;
-	[Command]
-	public void SetLineColor(Gradient colorGradient)
-	{
-		lineRenderer.colorGradient = colorGradient;
-	}
-	[Command]
-	public void SetLinePointsMinDistance(float distance)
-	{
-		linePointsMinDistance = distance;
-	}
-	[Command]
-	public void SetLineWidth(float width)
-	{
-		lineRenderer.startWidth = width;
-		lineRenderer.endWidth = width;
-	}
-	[Command]
-	public void CreateLine()
-	{
-		fingerPositions.Clear();
-		fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-		fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-		//lineRenderer.SetPosition(0, fingerPositions[0]);
-		//lineRenderer.SetPosition(1, fingerPositions[1]);
-		edgeCollider.points = fingerPositions.ToArray();
-		edgeColliderTriggerable.points = fingerPositions.ToArray();
-	}
-	[Command]
-	public void AddPoint(Vector2 newFingerPosition)
-	{
-		if (pointsCount >= 1 && Vector2.Distance(newFingerPosition, fingerPositions[fingerPositions.Count - 1]) < linePointsMinDistance)
-			return;
 
-		fingerPositions.Add(newFingerPosition);
+	[ClientRpc]
+	public void drawBaseOnMouse(Vector2 mousePosition)
+	{
+		
+
 		pointsCount++;
-		lineRenderer.positionCount = pointsCount;
-		lineRenderer.SetPosition(pointsCount - 1, newFingerPosition);
+		gameObject.GetComponent<LineRenderer>().positionCount = pointsCount;
+		gameObject.GetComponent<LineRenderer>().SetPosition(pointsCount - 1, mousePosition);
+		fingerPositions.Add(mousePosition);
 
-		if (pointsCount > 1)
-			edgeCollider.points = fingerPositions.ToArray();
-			edgeColliderTriggerable.points = fingerPositions.ToArray();
+		gameObject.GetComponents<EdgeCollider2D>()[0].points = fingerPositions.ToArray();
+		gameObject.GetComponents<EdgeCollider2D>()[1].points = fingerPositions.ToArray();
+		
 	}
-
-	[Server]
-	public void DestroySelf()
-    {
-		Destroy(gameObject);
-		NetworkServer.Destroy(gameObject);
-    }
-
-	[Server]
-	public IEnumerator DestroySelfAfter(float seconds)
-    {
-		yield return new WaitForSeconds(seconds);
-		NetworkServer.Destroy(gameObject);
-	}
-
 
 }
+
--- Assets/Scripts/LineDrawer.cs
+++ Assets/Scripts/LineDrawer.cs
@@ -6,127 +6,38 @@ public class LineDrawer : NetworkBehaviour
 {
 	public LayerMask cantDrawOverLayer;
 	public GameObject linePrefab;
-	public Sprite redInkSprite;
-	public Sprite blueInkSprite;
-	public Sprite yellowInkSprite;
-	public Image leftSideInkImage;
-	public Image rightSideInkImage;
-	[HideInInspector] public Gradient lineColor;
-	public float pointsMinDistance;
-	public float lineWidth;
-	public float maximumLineLength;
-	public int maximumLinePoints;
-
 	GameObject currentLine;
-	int currentInk;
-
-	int yellowCurrentInk;
-
-	Color yellowInk = new Color(0.8352942f, 0.7568628f, 0.2235294f, 1.0f);
 
 	[Command]
-	public void YellowButton()
+	void BeginDraw()
 	{
-		lineColor.SetKeys(
-			new GradientColorKey[] { new GradientColorKey(yellowInk, 1.0f) },
-			new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 1.0f) }
-		);
-	}
-
-	[Command]
-	void CmdBeginDraw()
-	{
-		GameObject newline = Instantiate(linePrefab, this.transform);
-		NetworkServer.Spawn(newline,connectionToClient);
+		GameObject newline = Instantiate(linePrefab);
+		NetworkServer.Spawn(newline);
 		currentLine = newline;
-		//currentLine.SetLineColor(lineColor);
-		currentLine.GetComponent<Line>().SetLinePointsMinDistance(pointsMinDistance);
-		currentLine.GetComponent<Line>().SetLineWidth(lineWidth);
-		currentLine.GetComponent<Line>().CreateLine();
 	}
 
 	[Command]
-	void CmdDraw()
+	void Draw()
 	{
-
-		 if (lineColor.colorKeys[0].color == yellowInk)
-		{
-			currentInk = yellowCurrentInk;
-		}
-
-		if (currentInk > 0)
-		{
-			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			RaycastHit2D hit = Physics2D.CircleCast(mousePosition, lineWidth / 3f, Vector2.zero, 1f, cantDrawOverLayer);
-
-			if (hit)
-			{
-				CmdEndDraw();
-				
-			}
-			else
-				if (Vector2.Distance(currentLine.GetComponent<Line>().fingerPositions[0], currentLine.GetComponent<Line>().fingerPositions[currentLine.GetComponent<Line>().fingerPositions.Count - 1]) <= maximumLineLength && currentLine.GetComponent<Line>().pointsCount <= maximumLinePoints)
-            {
-				currentLine.GetComponent<Line>().AddPoint(mousePosition);
-			}
-					
-			
-		}
+		Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		currentLine.GetComponent<Line>().drawBaseOnMouse(mousePosition);
 	}
 
-	[Command]
-	void CmdEndDraw()
-	{
-		currentInk = 0;
-		if (currentLine != null)
-		{
-			if (currentLine.GetComponent<Line>().pointsCount < 2)
-			{
-				currentLine.GetComponent<Line>().DestroySelf();
-			}
-			else
-			{
-
-                currentLine.GetComponent<Line>().DestroySelfAfter(3);
-				currentLine.gameObject.layer = LayerMask.NameToLayer("Obstacle");
-				currentLine = null;
-			}
-		}
-		else
-        {
-			print("null ang current line");
-        }
-	}
-
-	void Start()
-	{
-		if(!isLocalPlayer)
-        {
-			return;
-        }
-		yellowCurrentInk = 100;
-		YellowButton();
-	}
 	
 	void Update()
 	{
-		if(!isLocalPlayer) { return; }
+		if(!hasAuthority) 
+		{ 
+			return; 
+		}
 
 		if (Input.GetMouseButtonDown(0))
         {
-			CmdBeginDraw();
+			BeginDraw();
 		}
-			
 
 		if (currentLine != null)
-			CmdDraw();
+			Draw();
 
-		if (Input.GetMouseButtonUp(0))
-			CmdEndDraw();
-
-		if (Input.GetKeyDown(KeyCode.X))
-        {
-			currentLine.transform.position = currentLine.transform.position + new Vector3(1, 0, 0);
-        }
-	}
+    }
 }
