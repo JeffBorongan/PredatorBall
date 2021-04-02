cde79204f7f9287d2422ea98410a33a459da61a9
JeffBorongan <jeffborongan@gmail.com>
2021-04-02 13:05:06 +0800

    [Ink - Jeff] - Reimplemented the color changing code
    Not yet networked

---
+++ Assets/Scripts/InkNetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Mirror;
+
+[AddComponentMenu("")]
+public class InkNetworkManager : NetworkManager
+{
+    public Vector2 spawnLimits;
+    GameObject ball;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        GameObject player = Instantiate(playerPrefab);
+        NetworkServer.AddPlayerForConnection(conn, player);
+
+        if (numPlayers == 2)
+        {
+            ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
+            Vector2 spawnPosition = new Vector2(-0.13f, Random.Range(-spawnLimits.y, spawnLimits.y));
+            ball.transform.position = spawnPosition;
+            NetworkServer.Spawn(ball);
+        }
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        if (ball != null)
+            NetworkServer.Destroy(ball);
+
+        base.OnServerDisconnect(conn);
+    }
+}
--- Assets/Scripts/LineDrawer.cs
+++ Assets/Scripts/LineDrawer.cs
@@ -6,24 +6,75 @@ public class LineDrawer : NetworkBehaviour
 {
 	public LayerMask cantDrawOverLayer;
 	public GameObject linePrefab;
+	public GameObject leftSideImage;
+	public GameObject rightSideImage;
+	public Sprite redInkSprite;
+	public Sprite blueInkSprite;
+	public Sprite yellowInkSprite;
+	[HideInInspector] public Gradient lineColor;
+	[SerializeField] private GameObject currentLine;
+	private Image leftSideInkImage;
+	private Image rightSideInkImage;
+	private Vector2 ServermousePosition;
+	private Color redInk = new Color(0.9490197f, 0.1882353f, 0.2039216f, 1.0f);
+	private Color blueInk = new Color(0.2235294f, 0.6f, 0.8352942f, 1.0f);
+	private Color yellowInk = new Color(0.8352942f, 0.7568628f, 0.2235294f, 1.0f);
 
-	[SerializeField]
-	private GameObject currentLine;
-	Vector2 ServermousePosition;
+	public void RedButton()
+	{
+		lineColor.SetKeys(
+			new GradientColorKey[] { new GradientColorKey(redInk, 1.0f) },
+			new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 1.0f) }
+		);
+
+		leftSideInkImage = Instantiate(leftSideImage.GetComponent<Image>());
+		leftSideInkImage.transform.SetParent(GameObject.FindGameObjectWithTag("Left Side Bottle").transform, false);
+		leftSideInkImage.sprite = redInkSprite;
+		leftSideInkImage.type = Image.Type.Filled;
+		leftSideInkImage.fillMethod = Image.FillMethod.Vertical;
+		leftSideInkImage.fillOrigin = 0;
+		leftSideInkImage.fillAmount = 100f;
+	}
+
+	public void BlueButton()
+	{
+		lineColor.SetKeys(
+			new GradientColorKey[] { new GradientColorKey(blueInk, 1.0f) },
+			new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 1.0f) }
+		);
+
+		leftSideInkImage = Instantiate(leftSideImage.GetComponent<Image>());
+		leftSideInkImage.transform.SetParent(GameObject.FindGameObjectWithTag("Left Side Bottle").transform, false);
+		leftSideInkImage.sprite = blueInkSprite;
+		leftSideInkImage.type = Image.Type.Filled;
+		leftSideInkImage.fillMethod = Image.FillMethod.Vertical;
+		leftSideInkImage.fillOrigin = 0;
+		leftSideInkImage.fillAmount = 100f;
+	}
+
+	public void YellowButton()
+	{
+		lineColor.SetKeys(
+			new GradientColorKey[] { new GradientColorKey(yellowInk, 1.0f) },
+			new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 1.0f) }
+		);
+
+		leftSideInkImage = Instantiate(leftSideImage.GetComponent<Image>());
+		leftSideInkImage.transform.SetParent(GameObject.FindGameObjectWithTag("Left Side Bottle").transform, false);
+		leftSideInkImage.sprite = yellowInkSprite;
+		leftSideInkImage.type = Image.Type.Filled;
+		leftSideInkImage.fillMethod = Image.FillMethod.Vertical;
+		leftSideInkImage.fillOrigin = 0;
+		leftSideInkImage.fillAmount = 100f;
+	}
 
 	[Command]
 	void BeginDraw()
 	{
 		GameObject newline = Instantiate(linePrefab);
 		NetworkServer.Spawn(newline);
-		setCurrentLine(newline);
+		SetCurrentLine(newline);
 	}
-
-	[ClientRpc]
-	public void setCurrentLine(GameObject newline)
-    {
-		currentLine = newline;
-    }
 
 	[Command]
 	void Draw()
@@ -32,13 +83,23 @@ public class LineDrawer : NetworkBehaviour
 	}
 
 	[Command]
-	void setServerMousePosition(Vector2 mouseposition)
+	void SetServerMousePosition(Vector2 mouseposition)
     {
 		ServermousePosition = mouseposition;
 	}
 
+	[ClientRpc]
+	public void SetCurrentLine(GameObject newline)
+	{
+		currentLine = newline;
+	}
 
-	
+	void Start()
+    {
+		//leftSideInkImage = Instantiate(leftSideImage.GetComponent<Image>());
+		//leftSideInkImage.transform.SetParent(GameObject.FindGameObjectWithTag("Left Side Bottle").transform, false);
+	}
+
 	void Update()
 	{
 		if(!hasAuthority) 
@@ -46,20 +107,17 @@ public class LineDrawer : NetworkBehaviour
 			return; 
 		}
 
-		Vector3 mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		setServerMousePosition(mouseposition);
-
+		//Vector3 mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		//SetServerMousePosition(mouseposition);
 
 		if (Input.GetMouseButtonDown(0))
         {
-			BeginDraw();
-			
+			//BeginDraw();
 		}
 
 		if (this.currentLine != null)
 		{
-			Draw();
+			//Draw();
 		}	
-
     }
 }
