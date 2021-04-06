2c6b64ea7b3f114d0eafad43a6800a78db34a036
Nivelle Mendiola <mendiolanivelle@gmail.com>
2021-04-07 03:12:32 +0800

    [Ink - Nivelle] - backend for the scoring system is now working, the only thing that is needed now is the UI

---
+++ Assets/InkPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class InkPlayer : NetworkBehaviour
+{
+    public GameObject PlayerUIPrefab;
+    GameObject playerUI;
+    // Events that the UI will subscribe to
+    public event System.Action<int> OnPlayerScored;
+
+    [SyncVar(hook = nameof(PlayerScored))]
+    public int playerScore = 100;
+
+    // This is called by the hook of playerScored SyncVar above
+    void PlayerScored(int _, int newScore)
+    {
+        OnPlayerScored?.Invoke(newScore);
+    }
+
+    public override void OnStartClient()
+    {
+        playerUI = Instantiate(PlayerUIPrefab,gameObject.transform);
+        playerUI.GetComponent<InkPlayerUI>().SetPlayer(this);
+
+        // Invoke all event handlers with the current data
+        OnPlayerScored.Invoke(playerScore);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            AddScore();
+            print("balbal");
+        }
+    }
+
+    [Server]
+    void AddScore()
+    {
+        playerScore = playerScore + 1;
+    }
+}
---
+++ Assets/InkPlayerUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class InkPlayerUI : MonoBehaviour
+{
+    public Text PlayerScore;
+
+
+    public void SetPlayer(InkPlayer player)
+    {
+        player.OnPlayerScored += OnPlayerScored;
+    }
+
+    private void OnPlayerScored(int score)
+    {
+        PlayerScore.text = score.ToString();
+    }
+}
